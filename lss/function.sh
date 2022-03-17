df2acb8929156039aeeaf7b025cf17364e4c5a05
=yathish <=yathish971@gmail.com>
2022-03-17 12:50:44 +0530

    addition function is added

--- lss/function.sh
+++ lss/function.sh
@@ -1,9 +1,19 @@
 
 function print(){
-val1=$1
-echo $val
 
+val1="YAthish"
+echo "$1"
+echo "$val1"
+}
+
+function add()
+{
+read -p "enter the a value " $a
+read -p "Enter the b value " $b
+
+echo `expr $a + $b`
 }
 #printing user argument 
 print $1
-echo $val
+add
+echo "$val"
